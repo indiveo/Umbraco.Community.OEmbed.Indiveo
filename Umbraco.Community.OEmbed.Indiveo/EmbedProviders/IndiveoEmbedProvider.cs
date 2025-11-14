846af7c1b8bc24345920ce6b2d20c1fba72f96c8
Nick Huitema <nick@indiveo.nl>
2025-11-14 14:50:37 +0100

    umbraco/16

--- Umbraco.Community.OEmbed.Indiveo/EmbedProviders/IndiveoEmbedProvider.cs
+++ Umbraco.Community.OEmbed.Indiveo/EmbedProviders/IndiveoEmbedProvider.cs
@@ -19,10 +19,6 @@ namespace Umbraco.Community.OEmbed.Indiveo.EmbedProviders
 
         public override Dictionary<string, string> RequestParams => new();
 
-        [Obsolete("Use GetMarkupAsync instead. This will be removed in Umbraco 15.")]
-        public override string? GetMarkup(string url, int maxWidth = 0, int maxHeight = 0)
-            => GetMarkupAsync(url, maxWidth, maxHeight, CancellationToken.None).GetAwaiter().GetResult();
-
         public override async Task<string?> GetMarkupAsync(string url, int? maxWidth, int? maxHeight, CancellationToken cancellationToken)
         {
             var requestUrl = base.GetEmbedProviderUrl(url, maxWidth, maxHeight);
--- readme.md
+++ readme.md
@@ -44,7 +44,7 @@ https://indiveo.services/embed/f1557bd7-1584-495a-aecd-827189d6a471
 
 | Requirement | Version |
 |--------------|---------|
-| **Umbraco CMS** | 15.x    |
+| **Umbraco CMS** | 16.x    |
 | **.NET SDK** | 9.0     |
 
 ---
